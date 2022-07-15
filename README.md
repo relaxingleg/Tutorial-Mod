1d4f88e24d7bac77bc9b154ad07f40118df9d7cc
relaxingleg <relaxinglegcontact@gmail.com>
2022-07-15 17:58:25 +0100

    Initialized Repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tutorial Mod
+
+This is the code for my youtube tutorial series on making a minecraft mod:
+https://www.youtube.com/playlist?list=PLMDWhd7MfizVWFAKIgjaYuKgPpMDltXKH
---
+++ src/main/java/com/relaxingleg/tutorial/TutorialMod.java
@@ -0,0 +1,36 @@
+package com.relaxingleg.tutorial;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
+
+    public static final String MOD_ID = "tutorial";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TutorialMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
