973aa48e439a35f52788bdc9af4eb656f56279fc
relaxingleg <relaxinglegcontact@gmail.com>
2022-08-22 17:37:17 +0100

    added recipes

--- src/main/java/com/relaxingleg/tutorial/init/ModBlocks.java
+++ src/main/java/com/relaxingleg/tutorial/init/ModBlocks.java
@@ -2,7 +2,6 @@ package com.relaxingleg.tutorial.init;
 
 import com.relaxingleg.tutorial.TutorialMod;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -17,6 +16,8 @@ public class ModBlocks {
     public static final DeferredRegister<Item> BLOCK_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = BLOCKS.register("sapphire_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)));
+    public static final RegistryObject<Block> SAPPHIRE_ORE = BLOCKS.register("sapphire_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE)));
 
     public static final RegistryObject<BlockItem> SAPPHIRE_BLOCK_ITEM = BLOCK_ITEMS.register("sapphire_block", () -> new BlockItem(SAPPHIRE_BLOCK.get(), new Item.Properties().tab(TutorialMod.MOD_STUFF)));
+    public static final RegistryObject<BlockItem> SAPPHIRE_ORE_ITEM = BLOCK_ITEMS.register("sapphire_ore", () -> new BlockItem(SAPPHIRE_ORE.get(), new Item.Properties().tab(TutorialMod.MOD_STUFF)));
 }
