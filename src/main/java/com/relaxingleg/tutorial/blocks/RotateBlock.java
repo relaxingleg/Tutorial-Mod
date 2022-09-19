36d520f84e7c4365c6504d830e37a8438f144b37
relaxingleg <relaxinglegcontact@gmail.com>
2022-09-19 21:07:18 +0100

    Added blockstates and block rotation

---
+++ src/main/java/com/relaxingleg/tutorial/blocks/RotateBlock.java
@@ -0,0 +1,41 @@
+package com.relaxingleg.tutorial.blocks;
+
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import org.jetbrains.annotations.Nullable;
+
+public class RotateBlock extends Block {
+
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
+    public RotateBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
--- src/main/java/com/relaxingleg/tutorial/init/ModBlocks.java
+++ src/main/java/com/relaxingleg/tutorial/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.relaxingleg.tutorial.init;
 
 import com.relaxingleg.tutorial.TutorialMod;
+import com.relaxingleg.tutorial.blocks.RotateBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -17,7 +18,9 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = BLOCKS.register("sapphire_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)));
     public static final RegistryObject<Block> SAPPHIRE_ORE = BLOCKS.register("sapphire_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE)));
+    public static final RegistryObject<Block> ROTATE_BLOCK = BLOCKS.register("rotate_block", () -> new RotateBlock(BlockBehaviour.Properties.copy(Blocks.DISPENSER)));
 
     public static final RegistryObject<BlockItem> SAPPHIRE_BLOCK_ITEM = BLOCK_ITEMS.register("sapphire_block", () -> new BlockItem(SAPPHIRE_BLOCK.get(), new Item.Properties().tab(TutorialMod.MOD_STUFF)));
     public static final RegistryObject<BlockItem> SAPPHIRE_ORE_ITEM = BLOCK_ITEMS.register("sapphire_ore", () -> new BlockItem(SAPPHIRE_ORE.get(), new Item.Properties().tab(TutorialMod.MOD_STUFF)));
+    public static final RegistryObject<BlockItem> ROTATE_BLOCK_ITEM = BLOCK_ITEMS.register("rotate_block", () -> new BlockItem(ROTATE_BLOCK.get(), new Item.Properties().tab(TutorialMod.MOD_STUFF)));
 }
